c54c37fa54fe6eb95ea01a8faef8d22fce25d1f3
Cynthia Padilha <cynthiarfsilva@gmail.com>
2023-03-28 23:24:59 -0300

    ex javascript

--- cursob7/aula13/script.js
+++ cursob7/aula13/script.js
@@ -1,18 +1,6 @@
-let profissao = 'policial'
-
-console.log ('Profissão: '+ profissao)
-
-switch (profissao) {
-    case 'fiscal':
-        console.log ('Sua camisa será VERDE')
-        break;
-    case 'bombeiro':
-        console.log ('Sua camisa será VERMELHA')
-        break;
-    case 'policial':
-        console.log ('Sua camisa será AZUL')
-        break;
-    default:
-        console.log ('Sua camisa será BRANCA');
-        break;
+function gravidade () {
+    console.log ('A gravidade do planeta é:');
+    console.log(9.8);
 }
+
+gravidade()
---
+++ cursob7/aula14/script.js
@@ -0,0 +1,12 @@
+/*function soma (n1,n2){
+    let resultado = n1 + n2;
+    console.log('Resultado: ' + resultado)
+}
+soma(15,15)*/
+
+function nomeCompleto (nome,sobrenome){
+    console.log(`${nome} ${sobrenome}`)
+}
+nomeCompleto("Cynthia","Padilha") /* String por parenteses*/
+
+nomeCompleto('Michael','Padilha')
