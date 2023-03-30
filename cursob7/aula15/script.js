a4b9690c5f0e9feae77021ed5c5c664f7340efb4
Cynthia Padilha <cynthiarfsilva@gmail.com>
2023-03-29 23:25:19 -0300

    ex

---
+++ cursob7/aula15/script.js
@@ -0,0 +1,42 @@
+/*function maiorDeIdade (idade){
+    if (idade >= 18){
+        return true;
+    }else{
+        return false;
+    }
+}
+*/
+
+/*console.log (maiorDeIdade(10)) // posso mostrar o resultado dessa forma*/
+
+//let verificacao = maiorDeIdade(40); //ou dessa forma
+
+/*if (verificacao){
+    console.log ('É maior de idade');
+}else {
+    console.log ('É menor de idade');
+}
+*/
+
+function gravidade(num){
+    let resultado = num
+    return num
+}
+let num = 9.8
+
+let resultado = gavidade (num)
+
+
+
+console.log (resultado)
+
+
+/*function calcPct( x , y ){
+    let pct = (y / x)*100
+    return pct
+}
+let x =40 
+let y=10
+
+let pct = calcPct (x , y)
+console.log(`${pct}% de ${x} é ${y}`)
---
+++ cursob7/aula16/script.js
@@ -0,0 +1,13 @@
+function calcPct( x , y ){
+    let pct = (y / x)*100
+    return pct
+}
+let x =40 
+let y=10
+
+let pct = calcPct (x , y)
+console.log(`${pct}% de ${x} é ${y}`)
+
+
+//console.log (calcPct (40 ,10))
+
